7bbe5a319b9602fcc962984a58065649b58793c3
JoseCastanoCoding <j.albertocast@gmail.com>
2022-02-01 00:07:37 +0000

    056 deployment part 2

--- the_record_bloke/settings.py
+++ the_record_bloke/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-138_p91%sy!f$!j#p9^%$&ev-ln2*2^!c00x(f=_fw)+b5&^ao
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jose-the-record-bloke.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -118,12 +118,17 @@ WSGI_APPLICATION = 'the_record_bloke.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
