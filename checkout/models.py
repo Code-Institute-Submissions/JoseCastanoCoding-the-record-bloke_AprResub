b0e118562bdddc292d212674be300be75f9c0a9a
JoseCastanoCoding <j.albertocast@gmail.com>
2022-03-25 21:45:15 +0000

    changed checkout model

--- checkout/models.py
+++ checkout/models.py
@@ -11,7 +11,10 @@ from profiles.models import UserProfile
 
 
 class Order(models.Model):
-    order_number = models.CharField(max_length=32, null=False, editable=False)
+    """
+    Model to capture order confirmation
+    """
+    order_number = models.CharField(max_length=200, null=False, editable=False)
     user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
                                      null=True, blank=True, related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
