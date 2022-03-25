a5c63eee11c007e07ce57742f9748c89e23b1a61
JoseCastanoCoding <j.albertocast@gmail.com>
2022-03-25 22:15:30 +0000

    changed max length of stripe_pid

--- checkout/models.py
+++ checkout/models.py
@@ -14,7 +14,7 @@ class Order(models.Model):
     """
     Model to capture order confirmation
     """
-    order_number = models.CharField(max_length=200, null=False, editable=False)
+    order_number = models.CharField(max_length=32, null=False, editable=False)
     user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
                                      null=True, blank=True, related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
@@ -31,7 +31,7 @@ class Order(models.Model):
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     original_bag = models.TextField(null=False, blank=False, default='')
-    stripe_pid = models.CharField(max_length=200, null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
 
     def _generate_order_number(self):
