49d0bf9f02fe43a9ed270c8159f5e3a52c573c08
JoseCastanoCoding <j.albertocast@gmail.com>
2022-03-25 22:06:45 +0000

    hidden secret key from main settings

--- the_record_bloke/settings.py
+++ the_record_bloke/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-138_p91%sy!f$!j#p9^%$&ev-ln2*2^!c00x(f=_fw)+b5&^ao'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['jose-the-record-bloke.herokuapp.com', 'localhost']
 
